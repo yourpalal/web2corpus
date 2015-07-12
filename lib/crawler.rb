651e9867aa6ec94c0a020d4515683fcb4370d112
Alex Wilson <aewilson@ualberta.ca>
2015-07-11 21:07:55 -0600

    Add + test plaintext conversion

---
+++ lib/crawler.rb
@@ -0,0 +1,19 @@
+
+class Crawler
+  def initialize(crawl, query="body")
+    @crawl = crawl
+    @query = query
+  end
+
+  def doc_as_plaintext(doc)
+    # just using inner_text doesn't give us quite enough spaces :(
+    doc.css(@query).collect do |j|
+      bits = []
+      j.traverse do |c|
+        if c.text? then bits.push c.content end
+      end
+
+      bits.join(' ')
+    end.join(' ')
+  end
+end
---
+++ spec/crawler_spec.rb
@@ -0,0 +1,38 @@
+require 'crawler'
+require 'nokogiri'
+
+RSpec.describe Crawler, 'process_doc' do
+  before(:all) do
+    @root = "http://example.com"
+    @crawl = Crawl.new @root
+
+    @h1_content = "This is a document"
+    @p_content = "good stuff!"
+
+    @example_html = Nokogiri::HTML "<!doctype html><html><head></head><body><h1>#{@h1_content}</h1><p>#{@p_content}</p></body></html>"
+  end
+
+  before(:each) do
+    @crawler = Crawler.new @crawl
+  end
+
+  it 'can consider a page and make output' do
+    out = @crawler.doc_as_plaintext @example_html
+    expect(out).to eq "#{@h1_content} #{@p_content}"
+  end
+
+  it 'can limit the output by using css queries' do
+    tests = [
+      ["p", @p_content],
+      ["h1", @h1_content],
+      ["p, h1", "#{@h1_content} #{@p_content}"],
+      ["h1, p", "#{@h1_content} #{@p_content}"]
+    ]
+
+    tests.each do |test|
+      @crawler = Crawler.new @crawl, test[0]
+      out = @crawler.doc_as_plaintext @example_html
+      expect(out).to eq(test[1]), "with css query '#{test[0]}', got '#{out}', but expected '#{test[1]}'"
+    end
+  end
+end
